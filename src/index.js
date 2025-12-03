a48548de25f87d1e06ab9bee0e332081929cc326
Xin Li <happyxin331@icloud.com>
2025-12-03 11:38:01 -0800

    Partially complete wave 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 "use strict"
+// const axios = require('axios');
+const state = {
+    temp: 10,
+}
+
+const addTemp = () => {
+    const clickTempContainer = document.querySelector('#tempValue');
+    state.temp += 1;
+    clickTempContainer.textContent = state.temp;
+}
+
+const decreaseTemp = () => {
+    const clickTempContainer = document.querySelector('#tempValue');
+    state.temp -= 1;
+    clickTempContainer.textContent = state.temp;
+}
+
+const registerEventHandlers = () => {
+    const addTempretureButton = document.querySelector('#increaseTempControl');
+    addTempretureButton.addEventListener('click', addTemp);
+    const decreaseTempretureButton = document.querySelector('#decreaseTempControl');
+    decreaseTempretureButton.addEventListener('click', decreaseTemp);
+}
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
