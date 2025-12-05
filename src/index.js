6a2e45d7742849916c510db2f748c190e80af25e
Xin Li <happyxin331@icloud.com>
2025-12-05 01:58:05 -0800

    complete wave 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const state = {
     temp: 78,
+    city: 'Seattle',
 }
 const clickTempContainer = document.querySelector("#tempValue");
 const updateLandscapeContainer = document.getElementById("landscape");
@@ -20,28 +21,27 @@ const changeColor = () => {
     if (state.temp >= 80) {
         clickTempContainer.style.backgroundColor = "red";
         updateLandscapeContainer.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
-    } else if (state.temp>=70 && state.temp <= 79) {
-         clickTempContainer.style.backgroundColor = "orange";
-         updateLandscapeContainer.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
-    }else if (state.temp>=60 && state.temp <= 69) {
-         clickTempContainer.style.backgroundColor = "yellow";
-          updateLandscapeContainer.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
-    }else if(state.temp>=50 && state.temp <= 59) {
-         clickTempContainer.style.backgroundColor = "green";
-          updateLandscapeContainer.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
-    }else {
-         clickTempContainer.style.backgroundColor = "teal";
-          
+    } else if (state.temp >= 70 && state.temp <= 79) {
+        clickTempContainer.style.backgroundColor = "orange";
+        updateLandscapeContainer.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+    } else if (state.temp >= 60 && state.temp <= 69) {
+        clickTempContainer.style.backgroundColor = "yellow";
+        updateLandscapeContainer.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+    } else if (state.temp >= 50 && state.temp <= 59) {
+        clickTempContainer.style.backgroundColor = "green";
+        updateLandscapeContainer.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+    } else {
+        clickTempContainer.style.backgroundColor = "teal";
     }
 };
 
 const updateCity = () => {
     let cityValue = document.getElementById('cityNameInput').value;
-    if (cityValue){
+    state.city = cityValue;
+    if (cityValue) {
         document.getElementById('headerCityName').textContent = cityValue;
-    }else {
+    } else {
         document.getElementById('headerCityName').textContent = '';
-
     }
 }
 
@@ -53,67 +53,58 @@ const registerEventHandlers = () => {
     decreaseTempretureButton.addEventListener('click', decreaseTemp);
     decreaseTempretureButton.addEventListener('click', changeColor);
     const cityInput = document.getElementById('cityNameInput');
-    cityInput.addEventListener('input', updateCity)
-
+    cityInput.addEventListener('input', updateCity);
+    const realTimeTempButton =document.getElementById('currentTempButton');
+    realTimeTempButton.addEventListener('click', () => getWeatherFromLocation(state.city));
 }
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
 
-const axios = require('axios');
-const dotEnv = require('dotenv');
-
-dotEnv.config();
-const locationApiKey = process.env.LOCATION_KEY;
-const weatherApiKey = process.env.WEATHER_KEY;
-
 const getWeatherFromLocation = (query) => {
-    findLatitudeAndLongitude(query)
-    .then((response) => {
-        getWeather(response.latitude, response.long);
-    })
-    .catch((error) => {
-        console.log('error fetching loction from query')
-    })
+    return findLatitudeAndLongitude(query)
+        .then((response) => {
+            return getWeather(response.latitude, response.longitude);
+        })
+        .catch((error) => {
+            console.log('error fetching loction from query')
+        })
 };
 
 const findLatitudeAndLongitude = (query) => {
-  let latitude, longitude;
-  return axios.get('https://us1.locationiq.com/v1/search.php',
-    {
-      params: {
-        key: locationApiKey,
-        q: query,
-        format: 'json'
-      }
+    let latitude, longitude;
+    return axios.get('http://127.0.0.1:5000/location', {
+        params: {
+            q: query
+        }
     })
-    .then((response) => {
-      latitude = response.data[0].lat;
-      longitude = response.data[0].lon;
-      console.log('success in findLatitudeAndLongitude!', latitude, longitude);
-
-      return {latitude, longitude}; // Return the data we want to pass on
-    })
-    .catch((error) => {
-      console.log('error in findLatitudeAndLongitude!');
-      // console.log(error); // If we want to see more info about the issue
-    });
+        .then((response) => {
+            latitude = response.data[0].lat;
+            longitude = response.data[0].lon;
+            console.log('success in findLatitudeAndLongitude!', latitude, longitude);
+            return { latitude, longitude };
+        })
+        .catch((error) => {
+            console.log('error in findLatitudeAndLongitude!');
+        });
 };
 
-const getWeather = (latitude,longitude) =>{
-    return axios.get('https://api.openweathermap.org/data/2.5/onecall',
+const getWeather = (latitude, longitude) => {
+    return axios.get('http://127.0.0.1:5000/weather',
         {
             params: {
-                appid: weatherApiKey,
-                format: 'json',
                 lat: latitude,
                 lon: longitude
             }
         })
         .then((response) => {
-            console.log(response.current.temp);
-            return response;
+            const tempK = response.data.main.temp;
+            const tempF =  ( tempK - 273.15) * (9 / 5) + 32;
+            state.temp = Math.round(tempF);
+            clickTempContainer.textContent = state.temp;
+            changeColor();
+            return state.temp;
         })
         .catch((error) => {
             return error;
-        }); 
+        });
 };
